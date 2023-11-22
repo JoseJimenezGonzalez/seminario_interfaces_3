4d3e6a9a797b7d4d3f6a1018840fe9660c01b2fe
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-11-22 18:37:23 +0100

    commit ok

--- app/src/main/java/com/example/seminario/MainActivity4.kt
+++ app/src/main/java/com/example/seminario/MainActivity4.kt
@@ -1,9 +1,14 @@
 package com.example.seminario
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.seminario.databinding.ActivityMain2Binding
 import com.example.seminario.databinding.ActivityMain4Binding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.regex.Pattern
 
 class MainActivity4 : AppCompatActivity() {
 
@@ -14,5 +19,51 @@ class MainActivity4 : AppCompatActivity() {
 
         binding = ActivityMain4Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupBotones()
+    }
+
+    private fun setupBotones() {
+        binding.tietFechaNacimiento.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+            val picker = builder.build()
+
+            picker.addOnPositiveButtonClickListener {
+                binding.tietFechaNacimiento.setText(picker.headerText)
+            }
+
+            picker.show(supportFragmentManager, "jeje")
+        }
+
+
+
+        binding.btnEnviar.setOnClickListener {
+
+        }
+
+        binding.tietPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No es necesario implementar este método para tu caso
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Este método se llama cuando el texto cambia
+                val password = s.toString()
+                // Aquí puedes realizar la evaluación de la contraseña con una expresión regular
+                val pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}\$")
+                val matcher = pattern.matcher(password)
+
+                if (!matcher.matches()) {
+                    binding.tilPassword.error =
+                        "La contraseña debe tener al menos 8 caracteres, un número, una letra mayúscula y una minúscula."
+                } else {
+                    binding.tilPassword.error = null // Borra cualquier mensaje de error
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // No es necesario implementar este método para tu caso
+            }
+        })
     }
 }
