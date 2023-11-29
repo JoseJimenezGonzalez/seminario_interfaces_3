888acb7a3a0d0c0b6563c6dc4d4b417da77b164a
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-11-29 18:44:27 +0100

    commit ok

--- app/src/main/java/com/example/seminario/MainActivity4.kt
+++ app/src/main/java/com/example/seminario/MainActivity4.kt
@@ -1,18 +1,25 @@
 package com.example.seminario
 
-import android.graphics.Color
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import com.example.seminario.databinding.ActivityMain2Binding
+import android.widget.Toast
 import com.example.seminario.databinding.ActivityMain4Binding
 import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.Calendar
+import java.util.Date
 import java.util.regex.Pattern
 
 class MainActivity4 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain4Binding
+
+    var passIsValid = false
+    var emailIsValid = false
+    var postalCodeIsValid = false
+    var ageIsValid = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,24 +30,49 @@ class MainActivity4 : AppCompatActivity() {
         setupBotones()
     }
 
+    // Función para verificar si la persona es mayor de edad
+    fun isMayorDeEdad(fechaNacimiento: Date, fechaActual: Date): Boolean {
+        val edadMinima = 18 // La edad mínima para ser considerado mayor de edad
+
+        val calendarNacimiento = Calendar.getInstance().apply { time = fechaNacimiento }
+        val calendarActual = Calendar.getInstance().apply { time = fechaActual }
+
+        val aniosDiferencia = calendarActual.get(Calendar.YEAR) - calendarNacimiento.get(Calendar.YEAR)
+
+        // Verificar si la persona tiene al menos la edad mínima
+        return if (calendarActual.get(Calendar.DAY_OF_YEAR) < calendarNacimiento.get(Calendar.DAY_OF_YEAR)) {
+            aniosDiferencia - 1 >= edadMinima
+        } else {
+            aniosDiferencia >= edadMinima
+        }
+    }
+
     private fun setupBotones() {
+
+        //Fecha de nacimiento
         binding.tietFechaNacimiento.setOnClickListener {
             val builder = MaterialDatePicker.Builder.datePicker()
             val picker = builder.build()
 
-            picker.addOnPositiveButtonClickListener {
-                binding.tietFechaNacimiento.setText(picker.headerText)
+            picker.addOnPositiveButtonClickListener { selectedDateInMillis ->
+                val selectedDate = Date(selectedDateInMillis)
+                val currentDate = Date()
+
+                if (isMayorDeEdad(selectedDate, currentDate)) {
+                    // La persona es mayor de edad, realiza la acción correspondiente
+                    binding.tietFechaNacimiento.setText(picker.headerText)
+                    ageIsValid = true
+                } else {
+                    // La persona no es mayor de edad, puedes mostrar un mensaje o realizar otra acción
+                    Toast.makeText(this, "Debes ser mayor de edad", Toast.LENGTH_SHORT).show()
+                    ageIsValid = false
+                }
             }
 
             picker.show(supportFragmentManager, "jeje")
         }
 
-
-
-        binding.btnEnviar.setOnClickListener {
-
-        }
-
+        //Contraseña
         binding.tietPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // No es necesario en este caso
@@ -56,18 +88,17 @@ class MainActivity4 : AppCompatActivity() {
                 if (!matcher.matches()) {
                     binding.tilPassword.error =
                         "La contraseña debe tener al menos 8 caracteres, un número, una letra mayúscula y una minúscula."
+                    passIsValid = false
                 } else {
                     binding.tilPassword.error = null // Borra cualquier mensaje de error
+                    passIsValid = true
                 }
             }
-            //afterTextChanged(Editable s): Este método se llama para notificar que el texto en
-            //EditText ha cambiado completamente. Se ejecuta después de que el texto se ha
-            //modificado y onTextChanged ha sido llamado.
             override fun afterTextChanged(s: Editable?) {
                 // No es necesario en este caso
             }
         })
-
+        //Correo
         binding.tietCorreo.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // No es necesario en este caso
@@ -80,8 +111,10 @@ class MainActivity4 : AppCompatActivity() {
                 if (!password.contains("@") || !password.contains(".")) {
                     binding.tilCorreo.error =
                         "El correo debe contener una @ y un ."
+                    emailIsValid = false
                 } else {
                     binding.tilCorreo.error = null // Borra cualquier mensaje de error
+                    emailIsValid = true
                 }
             }
 
@@ -89,7 +122,7 @@ class MainActivity4 : AppCompatActivity() {
                 // No es necesario en este caso
             }
         })
-
+        //Codigo postal
         binding.tietCodigoPostal.addTextChangedListener(object: TextWatcher {
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -103,8 +136,10 @@ class MainActivity4 : AppCompatActivity() {
                 if(codigoPostal.length != 5){
                     binding.tilCodigoPostal.error =
                         "El codigo postal debe de tener exactamente 5 caracteres."
+                    postalCodeIsValid = false
                 }else{
                     binding.tilCodigoPostal.error = null // Borra cualquier mensaje de error
+                    postalCodeIsValid = true
                 }
             }
 
@@ -113,5 +148,29 @@ class MainActivity4 : AppCompatActivity() {
             }
 
         })
+
+        //Enviar
+        binding.btnEnviar.setOnClickListener {
+            //Vamos a ver si el codigo postal es valido o esta a null/blank
+            var resultadoCodigoPostal = ""
+            resultadoCodigoPostal = binding.tietCodigoPostal.text.toString()
+            if(!ageIsValid){
+                Toast.makeText(this, "La edad es obligatoria", Toast.LENGTH_SHORT).show()
+            }
+            if(!emailIsValid){
+                Toast.makeText(this, "El email es obligatorio", Toast.LENGTH_SHORT).show()
+            }
+            if(!passIsValid){
+                Toast.makeText(this, "La contraseña es obligatoria", Toast.LENGTH_SHORT).show()
+            }
+            if(resultadoCodigoPostal.isNotBlank() && !postalCodeIsValid){
+                Toast.makeText(this, "El codigo postal no es obligatorio, pero si se pone debe de ser correcto", Toast.LENGTH_SHORT).show()
+            }
+            if(ageIsValid && emailIsValid && passIsValid && (resultadoCodigoPostal.isBlank() || postalCodeIsValid)){
+                //Pasamos a la siguiente actividad
+                val intent = Intent(this@MainActivity4, MainActivity5::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
