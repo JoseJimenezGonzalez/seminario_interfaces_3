447d8c135054eca125fe6312f1be8e41560627c8
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-12-04 19:38:47 +0100

    commit ok

--- app/src/main/java/com/example/seminario/MainActivity2.kt
+++ app/src/main/java/com/example/seminario/MainActivity2.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.example.seminario.databinding.ActivityMain2Binding
 import com.example.seminario.databinding.ActivityMainBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.Calendar
+import java.util.Date
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -54,40 +57,56 @@ class MainActivity2 : AppCompatActivity() {
 
         })
 
-        // Edad
-        binding.tietEdad.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // No es necesario en este caso
-            }
+        //Fecha de nacimiento
+        binding.tietEdad.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+            val picker = builder.build()
 
-            override fun onTextChanged(edad: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (edad.isNullOrEmpty()){
-                    binding.tilEdad.error = null
+            picker.addOnPositiveButtonClickListener { selectedDateInMillis ->
+                val selectedDate = Date(selectedDateInMillis)
+                val currentDate = Date()
+
+                if (isMayorDeEdad(selectedDate, currentDate)) {
+                    // La persona es mayor de edad, realiza la acción correspondiente
+                    binding.tietEdad.setText(picker.headerText)
                     ageIsValid = true
-                } else if(edad.toString().toInt() >= 18){
-                    binding.tilEdad.error = null
-                    ageIsValid = true
-                }else{
-                    binding.tilEdad.error = "La edad tiene que ser mas de 18 si se introduce"
+                } else {
+                    // La persona no es mayor de edad, puedes mostrar un mensaje o realizar otra acción
+                    Toast.makeText(this, "Debes ser mayor de edad", Toast.LENGTH_SHORT).show()
                     ageIsValid = false
                 }
             }
 
-            override fun afterTextChanged(p0: Editable?) {
-                // No es necesario en este caso
-            }
-        })
+            picker.show(supportFragmentManager, "jeje")
+        }
 
 
         //Vemos si pasamos a la otra actividad
         binding.btnInsertar.setOnClickListener {
-            if(nameIsValid && ageIsValid){
+            //Vamos a ver si la edad es valido o esta a null/blank
+            var edadUser = binding.tietEdad.text.toString()
+            if(nameIsValid && (ageIsValid||edadUser.isBlank())){
                 //Pasamos a la siguiente actividad
                 val intent = Intent(this@MainActivity2, MainActivity5::class.java)
                 startActivity(intent)
             }else{
                 Toast.makeText(this, "Falla algo", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+    fun isMayorDeEdad(fechaNacimiento: Date, fechaActual: Date): Boolean {
+        val edadMinima = 18 // La edad mínima para ser considerado mayor de edad
+
+        val calendarNacimiento = Calendar.getInstance().apply { time = fechaNacimiento }
+        val calendarActual = Calendar.getInstance().apply { time = fechaActual }
+
+        val aniosDiferencia = calendarActual.get(Calendar.YEAR) - calendarNacimiento.get(Calendar.YEAR)
+
+        // Verificar si la persona tiene al menos la edad mínima
+        return if (calendarActual.get(Calendar.DAY_OF_YEAR) < calendarNacimiento.get(Calendar.DAY_OF_YEAR)) {
+            aniosDiferencia - 1 >= edadMinima
+        } else {
+            aniosDiferencia >= edadMinima
         }
     }
 }
