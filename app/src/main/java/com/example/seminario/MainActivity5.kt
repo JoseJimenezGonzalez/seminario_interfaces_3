46249336a87f570baa497c02f36bd477b41eed36
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-12-04 19:53:56 +0100

    commit ok

--- app/src/main/java/com/example/seminario/MainActivity5.kt
+++ app/src/main/java/com/example/seminario/MainActivity5.kt
@@ -1,11 +1,24 @@
 package com.example.seminario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seminario.databinding.ActivityMain4Binding
+import com.example.seminario.databinding.ActivityMain5Binding
 
 class MainActivity5 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain5Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
+
+        binding = ActivityMain5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnVolverPrincipal.setOnClickListener {
+            val intent = Intent(this@MainActivity5, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
